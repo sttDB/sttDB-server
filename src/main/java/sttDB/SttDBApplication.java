910e88c41492c057de8ef3a0dee21b0f9bb9d0bb
Marc Ribalta Gene <mariva1995@gmail.com>
2017-11-23 18:24:22 +0100

    Easy integration of mongodb

--- src/main/java/sttDB/SttDBApplication.java
+++ src/main/java/sttDB/SttDBApplication.java
@@ -1,19 +1,14 @@
 package sttDB;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class SttDBApplication implements CommandLineRunner{
+public class SttDBApplication{
 
     public static void main(String[] args) {
         SpringApplication.run(SttDBApplication.class, args);
     }
 
-    @Override
-    public void run(String... strings) throws Exception {
-
-    }
 }
