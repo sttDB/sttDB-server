b54bb400820764f8f6348750704ffb2060037f2b
Marc Ribalta Gene <mariva1995@gmail.com>
2017-11-23 13:35:36 +0100

    refactored line treatment and now saving sequence

--- src/main/java/sttDB/service/FastaParser.java
+++ src/main/java/sttDB/service/FastaParser.java
@@ -1,10 +1,13 @@
 package sttDB.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
+import sttDB.domain.Sequence;
+import sttDB.repository.SequenceRepository;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -16,6 +19,9 @@ import java.util.Scanner;
 @Controller
 public class FastaParser {
 
+    @Autowired
+    private SequenceRepository sequenceRepository;
+
     @RequestMapping(value = "/uploadFasta", method = RequestMethod.POST)
     public void treatFasta(MultipartHttpServletRequest request) throws IOException {
         saveReceivedFile(request);
@@ -26,32 +32,39 @@ public class FastaParser {
         Iterator<String> iterator = request.getFileNames();
         MultipartFile multiFile = request.getFile(iterator.next());
         multiFile.getContentType();
-        if(!multiFile.getContentType().equals("fasta")){
-            return;
-        }
-        File f = new File("./recievedFiles/fasta.fasta");
+        File f = new File("./receivedFiles/fasta.fasta");
         multiFile.transferTo(f);
     }
 
     private void parseFile() throws FileNotFoundException {
-        Scanner fastaScanner = new Scanner(new FileReader("./recievedFiles/fasta.fasta"));
-        String trinityId="";
-        while(fastaScanner.hasNextLine()){
+        Scanner fastaScanner = new Scanner(new FileReader("./receivedFiles/fasta.fasta"));
+        Sequence sequence = new Sequence();
+        String transcript = "";
+        while (fastaScanner.hasNextLine()) {
             String line = fastaScanner.nextLine();
-            if(line.startsWith(">")){
-                trinityId = insertNewSequence();
-            }else{
-                addSequenceInfo(trinityId);
-            }
+            transcript = treatLine(sequence, transcript, line);
         }
     }
 
-    private String insertNewSequence() {
-        return null;
+    private String treatLine(Sequence sequence, String transcript, String line) {
+        if (line.startsWith(">")) {
+            closeLastSequence(sequence, transcript);
+            insertNewSequence(sequence, line);
+        } else {
+            transcript+=line;
+        }
+        return transcript;
     }
 
-    private void addSequenceInfo(String trinityId) {
-        //need to see how to do it
+    private void closeLastSequence(Sequence sequence, String transcript) {
+        if(sequence.getTrinityId()!=null){
+            sequence.setTranscript(transcript);
+            sequenceRepository.save(sequence);
+        }
+    }
+
+    private void insertNewSequence(Sequence sequence, String line) {
+        //agafar trinity i longitud
     }
 
 }
