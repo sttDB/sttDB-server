413aa216023ed8a807da918782dbcd93beaca62c
Marc Ribalta Gene <mariva1995@gmail.com>
2017-11-24 10:08:28 +0100

    added experiment field

--- src/main/java/sttDB/repository/SequenceRepository.java
+++ src/main/java/sttDB/repository/SequenceRepository.java
@@ -4,6 +4,8 @@ import sttDB.domain.Sequence;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface SequenceRepository extends MongoRepository<Sequence, Long> {
-    public Sequence findByTrinityId(@Param("trinityId") String trinityId);
+    public List<Sequence> findByTrinityId(@Param("trinityId") String trinityId);
 }
