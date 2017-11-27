852b096f148e9e0d1faf41722965d5cabe77271f
Marc Ribalta Gene <mariva1995@gmail.com>
2017-11-27 12:25:15 +0100

    Introducing the storage exceptions

---
+++ src/main/java/sttDB/service/storageService/StorageException.java
@@ -0,0 +1,12 @@
+package sttDB.service.storageService;
+
+public class StorageException extends RuntimeException {
+
+    public StorageException(String message) {
+        super(message);
+    }
+
+    public StorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
