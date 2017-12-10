e77fbff21bc9d7a1834120ef644dcf7d8d2cbd61
Nector12 <rarquesval@gmail.com>
2017-12-10 18:56:08 +0100

    Empty feature to ensure that context is loaded in tests

---
+++ src/test/java/sttDB/steps/EmptyFeature.java
@@ -0,0 +1,14 @@
+package sttDB.steps;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+
+public class EmptyFeature {
+    @Given("^I do nothing$")
+    public void iDoNothing() throws Throwable {
+    }
+
+    @Then("^Everything is okay$")
+    public void everythingIsOkay() throws Throwable {
+    }
+}
