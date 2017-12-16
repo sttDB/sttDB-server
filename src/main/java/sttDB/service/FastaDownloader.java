3229d4bc4d64f2effe27b6221d18d0bc765f923e
Marc Ribalta Gene <mariva1995@gmail.com>
2017-12-16 17:23:48 +0100

    defined a downloader service

--- src/main/java/sttDB/service/FastaDownloader.java
+++ src/main/java/sttDB/service/FastaDownloader.java
@@ -1,21 +1,28 @@
 package sttDB.service;
 
 import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import sttDB.domain.Sequence;
+import sttDB.repository.SequenceRepository;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
+import java.util.Iterator;
 import java.util.List;
 
 @Controller
 public class FastaDownloader {
 
-    @RequestMapping(value = "/download", method = RequestMethod.GET)
-    public void download(@RequestParam("name") List<String> sequenceId, final HttpServletRequest request, final HttpServletResponse response) throws IOException {
+    @Autowired
+    private SequenceRepository sequenceRepository;
+
+    @RequestMapping(value = "/downloadFasta", method = RequestMethod.GET)
+    public void download(@RequestParam("ids") List<String> sequenceId, final HttpServletRequest request, final HttpServletResponse response) throws IOException {
         File file = createFasta(sequenceId);
 
         InputStream fileInputStream = new FileInputStream(file);
@@ -31,7 +38,15 @@ public class FastaDownloader {
         output.flush();
     }
 
-    private File createFasta(List<String> sequenceId) {
+    private File createFasta(List<String> sequenceId) throws IOException {
+        PrintWriter writer = new PrintWriter("searchedQuery.fasta", "UTF-8");
+        Iterator<String> stringIterator = sequenceId.iterator();
+        while(stringIterator.hasNext()){
+            List<Sequence> resultSequence = sequenceRepository.findByTrinityId(stringIterator.next());
+            writer.println(">" + resultSequence.get(0).getTrinityId() + " len= " + resultSequence.get(0).getLength());
+            writer.println(resultSequence.get(0).getTranscript());
+        }
+        writer.close();
         return new File("./searchedQuery.fasta");
     }
 }
