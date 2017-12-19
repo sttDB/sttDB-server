a3b0f5d2e3ef0b136662151d4f353cb2fee0b4c9
Nector12 <rarquesvall@gmail.com>
2017-12-19 14:18:14 +0100

    Created interface for file managing classes, for the future.

--- src/main/java/sttDB/controller/InterproUploadController.java
+++ src/main/java/sttDB/controller/InterproUploadController.java
@@ -1,11 +1,20 @@
 package sttDB.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
+import sttDB.service.FileManager;
 
 @Controller
 public class InterproUploadController {
+
+    private FileManager fileManager;
+
+    @Autowired
+    public InterproUploadController(FileManager fileManager) {
+        this.fileManager = fileManager;
+    }
 
     @PostMapping("/upload/interpro")
     public void processRequest(MultipartHttpServletRequest request) {
