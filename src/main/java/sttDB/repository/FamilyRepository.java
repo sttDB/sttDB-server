1a48a182994ff7db0f6d5172b77fdcc539aa6ac2
Nector12 <rarquesvall@gmail.com>
2017-12-19 21:21:54 +0100

    Families are stored and referenced by sequences

--- src/main/java/sttDB/repository/FamilyRepository.java
+++ src/main/java/sttDB/repository/FamilyRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(collectionResourceRel = "families", path = "families")
 public interface FamilyRepository extends MongoRepository <Family, Long>{
     public Family findByInterproId(@Param("interproId") String interproId);
 }
--- src/main/java/sttDB/service/InterproStorer.java
+++ src/main/java/sttDB/service/InterproStorer.java
@@ -1,6 +1,7 @@
 package sttDB.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import sttDB.domain.Family;
 import sttDB.domain.Sequence;
 import sttDB.repository.FamilyRepository;
@@ -8,6 +9,7 @@ import sttDB.repository.SequenceRepository;
 
 import java.util.List;
 
+@Service
 public class InterproStorer {
 
     @Autowired
