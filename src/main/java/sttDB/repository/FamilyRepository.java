43535e259b021b4d752edaef812bc58631798144
Nector12 <rarquesvall@gmail.com>
2018-01-02 11:10:49 +0100

    Corrected type of ID in sequence and family repositories

--- src/main/java/sttDB/repository/FamilyRepository.java
+++ src/main/java/sttDB/repository/FamilyRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-public interface FamilyRepository extends MongoRepository <Family, Long>{
-    public Family findByInterproId(@Param("interproId") String interproId);
+import java.math.BigInteger;
+
+public interface FamilyRepository extends MongoRepository<Family, BigInteger> {
+    Family findByInterproId(@Param("interproId") String interproId);
 }
--- src/main/java/sttDB/repository/SequenceRepository.java
+++ src/main/java/sttDB/repository/SequenceRepository.java
@@ -1,14 +1,18 @@
 package sttDB.repository;
 
-import sttDB.domain.Sequence;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
+import sttDB.domain.Sequence;
 
+import java.math.BigInteger;
 import java.util.List;
 
-public interface SequenceRepository extends MongoRepository<Sequence, Long> {
-    public List<Sequence> findByTrinityId(@Param("trinityId") String trinityId);
-    public List<Sequence> findByTranscript(@Param("transcript") String transcript);
-    public List<Sequence> findByTrinityIdLike(@Param("trinityId") String trinityId);
-    public List<Sequence> findByTrinityIdAndExperiment(@Param("trinityId") String trinityId, @Param("experiment") String experiment);
+public interface SequenceRepository extends MongoRepository<Sequence, BigInteger> {
+    List<Sequence> findByTrinityId(@Param("trinityId") String trinityId);
+
+    List<Sequence> findByTranscript(@Param("transcript") String transcript);
+
+    List<Sequence> findByTrinityIdLike(@Param("trinityId") String trinityId);
+
+    List<Sequence> findByTrinityIdAndExperiment(@Param("trinityId") String trinityId, @Param("experiment") String experiment);
 }
