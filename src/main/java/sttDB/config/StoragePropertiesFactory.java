395af868c36cba182c1b93baaebdfde3e0b64400
Nector12 <rarquesvall@gmail.com>
2018-01-15 17:57:05 +0100

    Class in charge of creating the StorageProperties correctly. If the environment variable is not set, then throws an exception.

---
+++ src/main/java/sttDB/config/StoragePropertiesFactory.java
@@ -0,0 +1,19 @@
+package sttDB.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import sttDB.service.storageService.StorageProperties;
+
+@Configuration
+public class StoragePropertiesFactory {
+
+    @Bean
+    public StorageProperties storageProperties() {
+        String location = System.getenv("FILES_DIR");
+        if (location == null)
+            throw new IllegalStateException("Environment variable <FILES_DIR> not set");
+        else
+            return new StorageProperties(location);
+    }
+
+}
