a177116d1edffc5cc253dfb396c9fa55e45dde37
Nector12 <rarquesvall@gmail.com>
2018-01-15 03:13:48 +0100

    Added equals() and hashCode() to LineItems

--- src/main/java/sttDB/service/interproServices/LineItems.java
+++ src/main/java/sttDB/service/interproServices/LineItems.java
@@ -11,4 +11,24 @@ public class LineItems {
         this.interproId = interproId;
         this.description = description;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        LineItems lineItems = (LineItems) o;
+
+        if (trinityID != null ? !trinityID.equals(lineItems.trinityID) : lineItems.trinityID != null) return false;
+        if (interproId != null ? !interproId.equals(lineItems.interproId) : lineItems.interproId != null) return false;
+        return description != null ? description.equals(lineItems.description) : lineItems.description == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = trinityID != null ? trinityID.hashCode() : 0;
+        result = 31 * result + (interproId != null ? interproId.hashCode() : 0);
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        return result;
+    }
 }
