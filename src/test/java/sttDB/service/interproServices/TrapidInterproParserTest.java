b07d5ab1ddb0c2e031aa39d631c90fd5d53b9dce
Nector12 <rarquesvall@gmail.com>
2018-01-15 03:03:33 +0100

    Refactoring tests

--- src/test/java/sttDB/service/interproServices/TrapidInterproParserTest.java
+++ src/test/java/sttDB/service/interproServices/TrapidInterproParserTest.java
@@ -1,5 +1,6 @@
 package sttDB.service.interproServices;
 
+import org.junit.Before;
 import org.junit.Test;
 import sttDB.exception.InterproParsingException;
 
@@ -11,16 +12,22 @@ import static org.hamcrest.collection.IsEmptyCollection.empty;
 
 public class TrapidInterproParserTest {
 
+    public static final String BASE_INTERPRO_DIR = "files/interpro-test-files/";
+    private TrapidInterproParser sut;
+
+    @Before
+    public void setUp() throws Exception {
+        sut = new TrapidInterproParser();
+    }
+
     @Test(expected = InterproParsingException.class)
     public void fileToParseNotSet_throwException() throws InterproParsingException {
-        TrapidInterproParser sut = new TrapidInterproParser();
         sut.parse();
     }
 
     @Test
     public void fileWithHeaderOnly_emptyList() throws InterproParsingException {
-        TrapidInterproParser sut = new TrapidInterproParser();
-        sut.setFileToParse(getResource("files/interpro-test-files/header-only.txt"));
+        sut.setFileToParse(getResource("header-only.txt"));
 
         List<LineItems> result = sut.parse();
 
@@ -29,15 +36,15 @@ public class TrapidInterproParserTest {
 
     @Test(expected = InterproParsingException.class)
     public void wrongFile_exception() throws InterproParsingException {
-        TrapidInterproParser sut = new TrapidInterproParser();
-        sut.setFileToParse(getResource("files/interpro-test-files/wrong-file.fasta"));
+        sut.setFileToParse(getResource("wrong-file.fasta"));
 
         List<LineItems> result = sut.parse();
     }
 
     private String getResource(String path) {
         try {
-            return getClass().getClassLoader().getResource(path).getFile();
+            return getClass().getClassLoader()
+                    .getResource(BASE_INTERPRO_DIR + path).getFile();
         } catch (NullPointerException e) {
             throw new NullPointerException("Test resource <" + path + "> not found");
         }
