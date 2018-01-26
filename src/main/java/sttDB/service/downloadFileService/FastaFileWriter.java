9eccea806b82bcc107f4a1fbd052a82a10694ec0
Marc Ribalta Gene <mariva1995@gmail.com>
2018-01-26 19:50:40 +0100

    changed the message to show in the exception

--- src/main/java/sttDB/service/downloadFileService/FastaFileWriter.java
+++ src/main/java/sttDB/service/downloadFileService/FastaFileWriter.java
@@ -10,7 +10,7 @@ public class FastaFileWriter implements FileWriter<Sequence> {
     @Override
     public void insertDataLine(PrintWriter writer, Sequence data) throws InvalidObjectException {
         if(data.getTrinityId() == null || data.getTranscript() == null) {
-            throw new InvalidObjectException("Sequence not found");
+            throw new InvalidObjectException("Sequence information not found");
         }
         writer.println(">" + data.getTrinityId()
                 + " " + data.getDynamicFastaInfo());
