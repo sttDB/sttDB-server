e869cd3ad453de8645b99c11173d43aaf4dac92d
Marc Ribalta Gene <mariva1995@gmail.com>
2018-01-26 18:12:15 +0100

    first test done

---
+++ src/test/java/sttDB/service/downloadFileService/FileCreatorTest.java
@@ -0,0 +1,43 @@
+package sttDB.service.downloadFileService;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import sttDB.domain.Sequence;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class FileCreatorTest {
+
+    Sequence sequence;
+    FileCreator<Sequence> fastaFileCreator;
+    FileWriter<Sequence> fastaWriter;
+
+    @Before
+    public void setUp() {
+        sequence = new Sequence();
+        sequence.setTrinityId("test");
+        sequence.setDynamicFastaInfo("bla");
+        sequence.setTranscript("ABC");
+        fastaFileCreator = new FileCreator<>("fasta");
+        fastaWriter = new FastaFileWriter();
+    }
+
+    @Test
+    public void createSimpleFile() throws IOException {
+        List<Sequence> sequenceList = new ArrayList<>();
+        sequenceList.add(sequence);
+        fastaFileCreator.createFile(sequenceList, fastaWriter);
+        BufferedReader fileReader = new BufferedReader(new FileReader(new File("searchedQuery.fasta")));
+        String actualLine = fileReader.readLine();
+        String expectedLine = ">test bla";
+        assertEquals(expectedLine, actualLine);
+        actualLine = fileReader.readLine();
+        expectedLine = "ABC";
+        assertEquals(expectedLine, actualLine);
+        fileReader.close();
+    }
+}
