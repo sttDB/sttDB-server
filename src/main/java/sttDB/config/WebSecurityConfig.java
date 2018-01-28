0726f97e524ecb58dbb78fbadf6fc1c1f469939b
Marc Ribalta Gene <mariva1995@gmail.com>
2018-01-28 13:32:10 +0100

    problems with the WebSecurity module solved

---
+++ src/main/java/sttDB/config/WebSecurityConfig.java
@@ -0,0 +1,23 @@
+package sttDB.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .anyRequest().permitAll()
+                .and()
+                .addFilterBefore(new CORSFilter(), ChannelProcessingFilter.class)
+                .csrf().disable();
+    }
+}
