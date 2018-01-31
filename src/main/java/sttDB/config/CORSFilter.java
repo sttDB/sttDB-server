561330ff0317b774f96b9147943e9949029c067a
Nector12 <rarquesvall@gmail.com>
2018-01-31 11:33:19 +0100

    removed import

--- src/main/java/sttDB/config/CORSFilter.java
+++ src/main/java/sttDB/config/CORSFilter.java
@@ -1,6 +1,5 @@
 package sttDB.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
