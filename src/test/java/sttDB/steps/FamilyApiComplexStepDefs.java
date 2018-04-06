0dd81b781c2763b4bc2c64590cc8f50215e52d5c
Marc Ribalta Gene <mariva1995@gmail.com>
2018-04-06 10:48:09 +0200

    defined a new testing class for the family complex uris

---
+++ src/test/java/sttDB/steps/FamilyApiComplexStepDefs.java
@@ -0,0 +1,57 @@
+package sttDB.steps;
+
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import sttDB.domain.Family;
+import sttDB.repository.FamilyRepository;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+public class FamilyApiComplexStepDefs {
+
+    @Autowired
+    private FamilyRepository familyRepository;
+
+    @Autowired
+    private StepDefs stepDefs;
+
+    @Given("^I have some families in the DataBase$")
+    public void iHaveTwoFamiliesInTheDataBase() {
+        Family families = new Family();
+        families.setInterproId("1");
+        families.setDescription("asd");
+        familyRepository.save(families);
+        Family families2 = new Family();
+        families.setInterproId("2");
+        families.setDescription("bnm");
+        familyRepository.save(families2);
+        Family families3 = new Family();
+        families.setInterproId("3");
+        families.setDescription("xcv");
+        familyRepository.save(families3);
+        Family families4 = new Family();
+        families.setInterproId("4");
+        families.setDescription("asd bnm");
+        familyRepository.save(families4);
+    }
+
+    @When("^I use the complex family route \"([^\"]*)\"$")
+    public void iUseFamilyRoute(String url) throws Throwable {
+        stepDefs.result = stepDefs.mockMvc.perform(
+                get(url)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print());
+    }
+
+    @And("^The family has the correct description \"([^\"]*)\"$")
+    public void theFamilyIsCorrect(String resultDescription) throws Throwable {
+        stepDefs.result.andExpect(jsonPath("$.content[0].description", is(resultDescription)))
+                .andExpect(jsonPath("$.totalElements", is(1)));
+    }
+}
