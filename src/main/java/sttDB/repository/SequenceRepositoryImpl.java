a1adc2107097ae6a9452b984db6e195c5c58011a
Marc Ribalta Gene <mariva1995@gmail.com>
2018-04-10 12:34:18 +0200

    optimized the interpro upload, now doing a single update, before a double route was being done

---
+++ src/main/java/sttDB/repository/SequenceRepositoryImpl.java
@@ -0,0 +1,30 @@
+package sttDB.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import sttDB.domain.Experiment;
+import sttDB.domain.Family;
+import sttDB.domain.Sequence;
+
+class SequenceRepositoryImpl implements CustomSequenceRepository {
+
+    @Autowired
+    MongoOperations mongoOperation;
+
+
+    @Override
+    public void sequenceFamiliesUpload(String trinityId, Experiment experiment, Family family) {
+
+        Query query = new Query();
+        query.addCriteria(Criteria.where("trinityId").is(trinityId).and("experiment").is(experiment.getName()));
+
+
+        Update update = new Update();
+        update.addToSet("domainInfo.families", family);
+
+        mongoOperation.updateMulti(query, update, Sequence.class);
+    }
+}
