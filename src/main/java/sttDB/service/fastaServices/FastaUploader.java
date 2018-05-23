24ec63c6a33d23ee888d09248a24cdd100efeb37
Marc Ribalta Gene <mariva1995@gmail.com>
2018-05-23 11:44:44 +0200

    created the uploader

--- src/main/java/sttDB/service/fastaServices/FastaUploader.java
+++ src/main/java/sttDB/service/fastaServices/FastaUploader.java
@@ -1,10 +1,12 @@
 package sttDB.service.fastaServices;
 
+import org.springframework.stereotype.Service;
 import sttDB.domain.Experiment;
 import sttDB.exception.FastaParsingException;
 
 import java.nio.file.Path;
 
+@Service
 public class FastaUploader {
 
     private FastaInfoSaver infoSaver;
@@ -16,6 +18,11 @@ public class FastaUploader {
     }
 
     public void treatFasta(Path fastaFile, Experiment experiment) {
-
+        try {
+            infoSaver.deleteOldSequences(experiment);
+            parser.parseFile(fastaFile, experiment);
+        } catch (Exception e) {
+            throw new FastaParsingException(e);
+        }
     }
 }
