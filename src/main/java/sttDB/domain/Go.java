89ec966b6fed4622b54b296f6d4bf73108ef7c2c
Marc Ribalta Gene <mariva1995@gmail.com>
2018-06-06 10:05:20 +0200

    added the Go entity

---
+++ src/main/java/sttDB/domain/Go.java
@@ -0,0 +1,79 @@
+package sttDB.domain;
+
+import org.springframework.data.annotation.Id;
+
+public class Go implements DomainInformation {
+
+    private String goType;
+
+    private String slimId;
+
+    private String goName;
+
+    private String inputAccession;
+
+    public Go(){}
+
+    public Go(String goType, String slimId, String goName, String inputAccession, String goId, String description) {
+        this.goType = goType;
+        this.slimId = slimId;
+        this.goName = goName;
+        this.inputAccession = inputAccession;
+        this.goId = goId;
+        this.description = description;
+    }
+
+    public String getGoType() {
+        return goType;
+    }
+
+    public void setGoType(String goType) {
+        this.goType = goType;
+    }
+
+    public String getSlimId() {
+        return slimId;
+    }
+
+    public void setSlimId(String slimId) {
+        this.slimId = slimId;
+    }
+
+    public String getGoName() {
+        return goName;
+    }
+
+    public void setGoName(String goName) {
+        this.goName = goName;
+    }
+
+    public String getInputAccession() {
+        return inputAccession;
+    }
+
+    public void setInputAccession(String inputAccession) {
+        this.inputAccession = inputAccession;
+    }
+
+    public String getGoId() {
+        return goId;
+    }
+
+    public void setGoId(String goId) {
+        this.goId = goId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Id
+    private String goId;
+
+    private String description;
+
+}
