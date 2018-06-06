ea8903920ec5e94db6202cf3db671ed24c8b2594
Marc Ribalta Gene <mariva1995@gmail.com>
2018-06-06 11:56:14 +0200

    created go parser exception

---
+++ src/main/java/sttDB/exception/GoParsingException.java
@@ -0,0 +1,7 @@
+package sttDB.exception;
+
+public class GoParsingException extends RuntimeException {
+    public GoParsingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
