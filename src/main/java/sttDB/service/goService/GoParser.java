31e41830be7196b204ae58d706bb298b23e93a92
Marc Ribalta Gene <mariva1995@gmail.com>
2018-06-07 09:42:12 +0200

    added the parseFile function with error controll

--- src/main/java/sttDB/service/goService/GoParser.java
+++ src/main/java/sttDB/service/goService/GoParser.java
@@ -3,14 +3,33 @@ package sttDB.service.goService;
 
 import org.springframework.stereotype.Service;
 import sttDB.domain.Experiment;
+import sttDB.exception.GoParsingException;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.nio.file.Path;
+import java.util.Scanner;
 
 @Service
 public class GoParser {
 
     void parseFile(Path filePath, Experiment experiment) {
-
+        Scanner goScanner = null;
+        try {
+            goScanner = new Scanner(new FileReader(filePath.toFile()));
+            while (goScanner.hasNextLine()) {
+                String line = goScanner.nextLine();
+                treatLine(line);
+            }
+        } catch (FileNotFoundException e) {
+            throw new GoParsingException("File not found: " + filePath, e);
+        } finally {
+            if (goScanner != null)
+                goScanner.close();
+        }
     }
 
+    private void treatLine(String line) {
+
+    }
 }
