31132915c80ea093c56a9ce771e9ca13cfcfc645
Marc Ribalta Gene <mariva1995@gmail.com>
2018-06-13 12:57:23 +0200

    added kegg path groups

--- src/main/java/sttDB/service/keggService/KeggSaver.java
+++ src/main/java/sttDB/service/keggService/KeggSaver.java
@@ -4,4 +4,22 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class KeggSaver {
+
+    private String firstPath;
+
+    private String secondPath;
+
+    private String thirdPath;
+
+    public void setFirstPath(String firstPath) {
+        this.firstPath = firstPath;
+    }
+
+    public void setSecondPath(String secondPath) {
+        this.secondPath = secondPath;
+    }
+
+    public void setThirdPath(String thirdPath) {
+        this.thirdPath = thirdPath;
+    }
 }
