99a1807c1802226ff578d94d116305525df07db3
Marc Ribalta Gene <mariva1995@gmail.com>
2018-06-14 12:37:57 +0200

    added new exception constructor

--- src/main/java/sttDB/exception/KeggParsingException.java
+++ src/main/java/sttDB/exception/KeggParsingException.java
@@ -4,4 +4,8 @@ public class KeggParsingException extends RuntimeException {
     public KeggParsingException(String message, Exception exception) {
         super(message, exception);
     }
+
+    public KeggParsingException(String message) {
+        super(message);
+    }
 }
