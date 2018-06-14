09c309f15179837a37e2c19e78e4e877fe45b88e
Marc Ribalta Gene <mariva1995@gmail.com>
2018-06-14 11:43:28 +0200

    added the kegg methods

--- src/main/java/sttDB/repository/CustomSequenceRepository.java
+++ src/main/java/sttDB/repository/CustomSequenceRepository.java
@@ -3,10 +3,12 @@ package sttDB.repository;
 import sttDB.domain.Experiment;
 import sttDB.domain.Family;
 import sttDB.domain.Go;
+import sttDB.domain.Kegg;
 
 import java.util.List;
 
 public interface CustomSequenceRepository {
     void sequenceFamiliesUpload(String trinityId, Experiment experiment, List<Family> family);
     void sequenceGoTermUpload(String trinityId, Experiment experiment, Go go);
+    void sequenceKeggTermUpload(String trinityId, Experiment experiment, Kegg kegg);
 }
