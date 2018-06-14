f3fcc9d5c33525350a745393935163255614aebe
Marc Ribalta Gene <mariva1995@gmail.com>
2018-06-14 11:52:54 +0200

    now saving information about the sequence

--- src/main/java/sttDB/service/keggService/KeggSaver.java
+++ src/main/java/sttDB/service/keggService/KeggSaver.java
@@ -1,9 +1,20 @@
 package sttDB.service.keggService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import sttDB.domain.Experiment;
+import sttDB.domain.Kegg;
+import sttDB.repository.SequenceRepository;
+
+import java.util.Arrays;
 
 @Service
 public class KeggSaver {
+
+    @Autowired
+    private SequenceRepository sequenceRepository;
+
+    private String keggId;
 
     private String firstPath;
 
@@ -21,5 +32,22 @@ public class KeggSaver {
 
     public void setThirdPath(String thirdPath) {
         this.thirdPath = thirdPath;
+    }
+
+    public void setKeggId(String keggId) {
+        this.keggId = keggId;
+    }
+
+    public void saveSequences(String[] sequences, Experiment experiment) {
+        if(keggIsNotDefined()){
+            throw new UnsupportedOperationException("Kegg data is not defined");
+        }
+        Kegg kegg = new Kegg(keggId, firstPath, secondPath, thirdPath);
+        Arrays.asList(sequences)
+                .forEach(sequence -> sequenceRepository.sequenceKeggTermUpload(sequence, experiment, kegg));
+    }
+
+    private boolean keggIsNotDefined() {
+        return keggId == null || firstPath == null || secondPath == null || thirdPath == null;
     }
 }
