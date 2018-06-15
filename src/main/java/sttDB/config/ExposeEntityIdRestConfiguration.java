0198fc448a4063160b6a34fade956e274ac682df
Marc Ribalta Gene <mariva1995@gmail.com>
2018-06-15 11:53:09 +0200

    exposing IDs

--- src/main/java/sttDB/config/ExposeEntityIdRestConfiguration.java
+++ src/main/java/sttDB/config/ExposeEntityIdRestConfiguration.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
 import sttDB.domain.Family;
+import sttDB.domain.Go;
+import sttDB.domain.Kegg;
 import sttDB.domain.Sequence;
 
 @Configuration
@@ -13,5 +15,7 @@ public class ExposeEntityIdRestConfiguration extends RepositoryRestConfigurerAda
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
         config.exposeIdsFor(Family.class);
         config.exposeIdsFor(Sequence.class);
+        config.exposeIdsFor(Go.class);
+        config.exposeIdsFor(Kegg.class);
     }
 }
