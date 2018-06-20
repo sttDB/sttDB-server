d2da51ba7d5993076669750778f75959d9816568
Marc Ribalta Gene <mariva1995@gmail.com>
2018-06-20 11:00:44 +0200

    defining the unitary testing for the Go file

---
+++ src/test/java/sttDB/service/goServices/UploadGoFileTest.java
@@ -0,0 +1,17 @@
+package sttDB.service.goServices;
+
+import org.junit.Test;
+
+public class UploadGoFileTest {
+
+
+    @Test
+    public void uploadingNormalFileGoesWell(){
+
+    }
+
+    @Test
+    public void uploadingWrongFile(){
+
+    }
+}
