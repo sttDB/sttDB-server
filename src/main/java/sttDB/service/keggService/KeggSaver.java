b612f750c73c5cc899375b417f82f67f3a5dedc2
Marc Ribalta Gene <mariva1995@gmail.com>
2018-06-22 10:18:38 +0200

    refactored kegg creation function

--- src/main/java/sttDB/service/keggService/KeggSaver.java
+++ src/main/java/sttDB/service/keggService/KeggSaver.java
@@ -58,23 +58,33 @@ public class KeggSaver {
     private Kegg saveKegg() {
         Kegg storedKegg = keggRepository.findByKeggId(keggId);
         if (storedKegg == null) {
-            storedKegg = new Kegg();
-            storedKegg.setKeggId(keggId);
-            storedKegg.setPath1(Arrays.asList(firstPath));
-            storedKegg.setPath2(Arrays.asList(secondPath));
-            storedKegg.setPath3(Arrays.asList(thirdPath));
+            storedKegg = createKegg();
         }else{
-            if (!storedKegg.getPath1().contains(firstPath)){
-                storedKegg.addPath1(firstPath);
-            }
-            if(!storedKegg.getPath2().contains(secondPath)){
-                storedKegg.addPath2(secondPath);
-            }
-            if (!storedKegg.getPath3().contains(thirdPath)){
-                storedKegg.addPath3(thirdPath);
-            }
+            addPathsToExistentKegg(storedKegg);
         }
         keggRepository.save(storedKegg);
         return storedKegg;
+    }
+
+    private Kegg createKegg() {
+        Kegg storedKegg;
+        storedKegg = new Kegg();
+        storedKegg.setKeggId(keggId);
+        storedKegg.setPath1(Arrays.asList(firstPath));
+        storedKegg.setPath2(Arrays.asList(secondPath));
+        storedKegg.setPath3(Arrays.asList(thirdPath));
+        return storedKegg;
+    }
+
+    private void addPathsToExistentKegg(Kegg storedKegg) {
+        if (!storedKegg.getPath1().contains(firstPath)){
+            storedKegg.addPath1(firstPath);
+        }
+        if(!storedKegg.getPath2().contains(secondPath)){
+            storedKegg.addPath2(secondPath);
+        }
+        if (!storedKegg.getPath3().contains(thirdPath)){
+            storedKegg.addPath3(thirdPath);
+        }
     }
 }
